7f64eb4a364694f91c74b65156e289200017590d
Allan Barnes <allanebarnes@gmail.com>
2018-06-27 15:27:44 -0400

    gcloud AGAIN

--- scripts/shared.js
+++ scripts/shared.js
@@ -11,11 +11,11 @@ const AUTH_URL = `${API_URL}/auth`
 function getHostURL() {
   if (window.location.host.indexOf('localhost') != -1) {
     //return 'http://localhost:3000';
-    return 'https://impact-kobo.appspot.com/';
+    return 'https://impact-kobo.appspot.com';
   } else {
     console.log('remote host');
     //return 'https://sticker-mania.herokuapp.com';
-    return 'https://impact-kobo.appspot.com/';
+    return 'https://impact-kobo.appspot.com';
   }
 }
 
